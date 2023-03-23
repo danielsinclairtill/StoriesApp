182ad4a7f45c5214db86f7bda70ab810d5b3d291
Daniel Till <daniel.sinclair.till@gmail.com>
2023-03-23 15:12:49 -0400

    default completion of URLSession to main thread

--- Stories/Stories/Model/API/StoriesNativeAPI.swift
+++ Stories/Stories/Model/API/StoriesNativeAPI.swift
@@ -34,9 +34,7 @@ class StoriesNativeAPI: APIContract {
     func get<R>(request: R, result: ((Result<R.Response, APIError>) -> Void)?) where R : APIRequestContract {
         guard var url = URLComponents(string: baseUrl + request.path) else {
             assertionFailure("url for api request was not formatted correctly")
-            DispatchQueue.main.async {
-                result?(.failure(.serverError))
-            }
+            result?(.failure(.serverError))
             return
         }
         
@@ -46,53 +44,40 @@ class StoriesNativeAPI: APIContract {
         
         guard let url = url.url else {
             assertionFailure("url for api request was not formatted correctly")
-            DispatchQueue.main.async {
-                result?(.failure(.serverError))
-            }
+            result?(.failure(.serverError))
             return
         }
         let urlRequest = URLRequest(url: url, timeoutInterval: request.timeoutInterval)
         
-        let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: urlRequest) { data, response, error in
             if let error = error as? URLError {
                 if error.code == .notConnectedToInternet || error.code == .timedOut {
-                    DispatchQueue.main.async {
-                        result?(.failure(.lostConnection))
-                    }
+                    result?(.failure(.lostConnection))
                 } else {
-                    DispatchQueue.main.async {
-                        result?(.failure(.serverError))
-                    }
+                    result?(.failure(.serverError))
                 }
                 return
             }
             
             if let response = response as? HTTPURLResponse, !(200...299).contains(response.statusCode) {
-                DispatchQueue.main.async {
-                    result?(.failure(.serverError))
-                }
+                result?(.failure(.serverError))
                 return
             }
             
             guard let data = data else {
-                DispatchQueue.main.async {
-                    result?(.failure(.serverError))
-                }
+                result?(.failure(.serverError))
                 return
             }
             
             do {
                 let decoder = JSONDecoder()
                 let data = try decoder.decode(R.Response.self, from: data)
-                DispatchQueue.main.async {
-                    result?(.success(data))
-                }
+                result?(.success(data))
             }
             catch {
                 assertionFailure("url for api request was not formatted correctly")
-                DispatchQueue.main.async {
-                    result?(.failure(.serverError))
-                }
+                result?(.failure(.serverError))
             }
         }
         task.resume()
