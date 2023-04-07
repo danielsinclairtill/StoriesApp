288eaf84f106780c3ab45e9b6f26fc9602a949af
Daniel Till <daniel.sinclair.till@gmail.com>
2023-04-07 18:08:22 -0400

    StoriesDesign re factor to inject state

--- Stories/Stories/View/DesignSystem/StoriesDesign.swift
+++ Stories/Stories/View/DesignSystem/StoriesDesign.swift
@@ -12,16 +12,18 @@ import RxSwift
 import RxCocoa
 
 class StoriesDesign {
-    static let shared = StoriesDesign(theme: ApplicationManager.shared.theme)
+    static let shared = StoriesDesign()
     var output: Output
     struct Output {
         let theme: Driver<StoriesDesignTheme>
     }
     private var design: BehaviorSubject<StoriesDesignTheme>?
     private(set) var theme: StoriesDesignTheme
+    private(set) var state: ApplicationStateContract
     
-    init(theme: StoriesDesignTheme = .plain) {
-        self.theme = theme
+    init(state: ApplicationStateContract = StoriesEnvironment.shared.state) {
+        self.theme = state.theme
+        self.state = state
 
         let design = BehaviorSubject<StoriesDesignTheme>(value: self.theme)
         self.design = design
@@ -31,7 +33,7 @@ class StoriesDesign {
     func changeToTheme(_ theme: StoriesDesignTheme) {
         self.theme = theme
         
-        ApplicationManager.shared.theme = theme
+        state.theme = theme
         design?.onNext(theme)
     }
 }
